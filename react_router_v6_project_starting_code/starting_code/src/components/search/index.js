278c4a01ef61a784522a5970924c2fa79d8a3ab8
sb-portfolio <sibond3@gmail.com>
2023-06-20 11:59:21 +0100

    react router first commit

---
+++ react_router_v6_project_starting_code/starting_code/src/components/search/index.js
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react';
+import {createSearchParams, useNavigate} from 'react-router-dom'
+
+const Search = () => {
+
+  // get navigate function
+  const navigate = useNavigate();
+
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    }
+
+    // use createSearchParams 
+    const query = createSearchParams(searchQuery)
+
+    // imperatively redirect with useNavigate() returned function
+    navigate(`/search?${query}`)
+
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
