6e0d9b10e58643f551cd07e7dbcc550bdaff9e24
sb-portfolio <sibond3@gmail.com>
2023-06-20 12:42:17 +0100

    add appointment states and contacts

--- appointment-planner-starting-v18/src/App.js
+++ appointment-planner-starting-v18/src/App.js
@@ -1,25 +1,53 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const [contacts, setContacts] = useState([])
+  const [appointments, setAppointments] = useState([])  
+
+  function addNewContact(name, phoneNumber, email){
+
+    const newContact = {
+      name,
+      phoneNumber,
+      email
+    }
+
+    setContacts(prevContacts => [newContact, ...prevContacts])
+  }
+
+  function addNewAppointment(name, contact, date, time){
+    
+    const newAppointment = {
+      name,
+      contact,
+      date,
+      time
+    }
+
+    setAppointments(prevAppointment => [newAppointment, ...prevAppointment])
+  }
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route path={ROUTES.CONTACTS} element={ 
+        <ContactsPage 
+          contacts={contacts} 
+          addNewContact={addNewContact}
+        />}
+      />
+      <Route path={ROUTES.APPOINTMENTS} element={ 
+        <AppointmentsPage  
+          contacts={contacts}
+          appointments={appointments} 
+          setAppointments={setAppointments}
+        />}
+      />
     </Route>
   ));
   
