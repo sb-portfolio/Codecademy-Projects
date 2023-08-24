8a5573b7fb082ff8aa44eb53bb1380c5f40613ae
sb-portfolio <sibond3@gmail.com>
2023-08-24 15:47:25 +0100

    add put api

--- quote-api-starting/server.js
+++ quote-api-starting/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const { quotes } = require('./data');
+let { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
@@ -65,8 +65,35 @@ quotesRouter.delete('/:id', (req, res, next) => {
     const quoteIndex = quotes.findIndex(quote => quote.id===quoteId)
 
     if(quoteIndex!==-1){
-        //quotes = quotes.filter(quote => quote.id!==quoteId)
         quotes.splice(quoteIndex,1)
+        res.status(201).send({
+            quotes: quotes
+        })
+    } else{
+        res.status(400).send()
+    }   
+})
+
+quotesRouter.put('/:id', (req, res, next) => {
+    const quoteId = req.params.id
+    const quoteIndex = quotes.findIndex(quote => quote.id===quoteId)
+    const quoteBody = req.query.quote
+    const personName = req.query.person
+
+    if(quoteIndex!==-1&&quoteBody&&personName){
+
+        quotes = quotes.map(quote => {
+            if(quote.id===quoteId){
+                return {
+                    ...quote,
+                    quote: quoteBody,
+                    person: personName
+                }   
+            } else{
+                return quote
+            }
+        })
+
         res.status(201).send({
             quotes: quotes
         })
