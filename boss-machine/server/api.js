1efc8de1463932f09a75dc85e5db2be03a235cb5
sb-portfolio <sibond3@gmail.com>
2023-09-05 16:02:28 +0100

    add minions get

--- boss-machine/server/api.js
+++ boss-machine/server/api.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const { getAllFromDatabase } = require('./db')
+
+apiRouter.get('/minions', (req, res, next) => {
+
+    const allMinionsArray = getAllFromDatabase('minions')
+
+    res.status(200).send(allMinionsArray)
+})
+
+apiRouter.post('/minions', (req, res, next) => {
+
+    console.log(req.body)
+    
+    res.status(200).send(allMinionsArray)
+})
+
+
 
 
 module.exports = apiRouter;
